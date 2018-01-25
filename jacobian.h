afa0849e7887282b1ccb2129937fc2784934836e
Alec Yen <ayen1@vols.utk.edu>
2018-01-25 16:15:02 -0500

    test

--- jacobian.h
+++ jacobian.h
@@ -1,12 +1,5 @@
 #include "cs.h"
 
-<<<<<<< HEAD
 typedef enum { Fx,Fy,Gx,Gy,Fx0,Fy0,Gx0,Gy0}  jac_name;
 
 static cs* jac_storage[8]; //never used
-=======
-typedef enum { Fx,Fy,Gx,Gy,Fx0,Fy0,Gx0,Gy0}  jac_mat;
-
-static cs* jac_storage[8];
->>>>>>> f0505432fb85798042bf71556faa924301592bb5
-
--- mysparse.h
+++ mysparse.h
@@ -11,17 +11,10 @@ void del(cs * A, int r, int c);
 void csc (const cs * A);
 void print (const cs * A);
 
-<<<<<<< HEAD
 /*Jacobian, returns TRIPLET matrices*/
 cs * add_jac (jac_name s, int* r, int* c, double* v, int size);
 cs * set_jac (jac_name s, int* r, int* c, double* v, int size);
 cs * alloc_jac (jac_name s, int m, int n);
-=======
-/*Jacobian of TRIPLET matrices*/
-cs* add_jac (jac_mat s, int* r, int* c, double* v, int size);
-cs* set_jac (jac_mat s, int* r, int* c, double* v, int size);
-//cs* acc_jac (jac_mat s, int m, int n);
->>>>>>> f0505432fb85798042bf71556faa924301592bb5
 
 /*not used*/
 cs * add(cs * A, int r, int c, double v);
